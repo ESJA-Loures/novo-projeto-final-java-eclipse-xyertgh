781a9dc8cd287422046a8c70e86e21d9b48cb20a
Ricardo Carvalho <ricardo.carvalho@CTED.ESJA>
2026-05-28 15:22:00 +0100

    Trabalho Final 20/20

--- src/Main.java
+++ src/Main.java
@@ -24,19 +24,21 @@ public class Main {
 			} else if (opcao == 2) {
     			System.out.println("Título: ");
     			String titulo = sc.nextLine();
+    			System.out.println("Ano: ");
+				int ano = sc.nextInt();
     
     			// TODO: Pede o ano ao utilizador (lê com sc.nextInt())
     			// int ano = sc.nextInt();
     
     			// NOTA: Limpa o buffer depois de leres o ano para evitar loops no menu
-    			sc.nextLine(); 
+    			sc.nextLine();
     
     			// TODO: Atualiza o construtor para passar também o ano
-    			Filme f = new Filme(titulo); 
+    			Filme f = new Filme(titulo, ano); 
     			v.adicionarFilme(f);
 			} else if (opcao == 3) {
 				// testar primeiro se há filmes para apagar
-				if (1 == 1) { // sustituir
+				if (v.totalFilmes() == 0) { // sustituir
 					System.out.println("Não há filmes para apagar.");
 				} else {
 					System.out.println("Número do filme a apagar: ");
@@ -51,7 +53,7 @@ public class Main {
 
 			} else if (opcao == 4) {
 				// testar primeiro se há filmes para editar, depois acrescentar o ano
-				if (1 == 1) { // substituir
+				if (v.totalFilmes() == 0) { // substituir
 					System.out.println("Não há filmes para editar.");
 				} else {
 					System.out.print("Número do filme a editar: ");
@@ -60,7 +62,9 @@ public class Main {
 					if (num >= 1 && num <= v.totalFilmes()) {
 						System.out.println("Novo titulo: ");
 						String novoTitulo = sc.nextLine();
-						v.editarFilme(num, novoTitulo);
+						System.out.println("Novo ano: ");
+						int novoAno = sc.nextInt();
+						v.editarFilme(num, novoTitulo, novoAno);
 					} else {
 						System.out.println("Numero inválido.");
 					}
@@ -70,6 +74,6 @@ public class Main {
 		} while (opcao != 0);
 
 		sc.close();
-		System.out.println("Ate logo!");
+		System.out.println("Até logo!");
 	}
 }
--- src/Videoteca.java
+++ src/Videoteca.java
@@ -14,31 +14,34 @@ public class Videoteca {
 
 	public void listarFilmes() {
 		// mostra o nome da videoteca
-		System.out.println("=== " + ("[Substituir] Nome da videoteca") + " ===");
+		System.out.println("=== " + ("Carvalhoteca") + " ===");
 		// Testa primeiro se a videoteca tem filmes
-		
+		if(!filmes.isEmpty()) {
 			for (int i = 0; i < filmes.size(); i++) {
 				System.out.println((i + 1) + ". " + filmes.get(i));
 			}
-			System.out.println("======");
-		
+		}else {
+			System.out.println("Não existem filmes disponíveis no momento :(");
+		}
+
+		System.out.println("====================");
 	}
 	
 	
 	public void adicionarFilme(Filme filme) {
-		System.out.println("Método por desenvolver");
+		filmes.add(filme);
 		// Completa o método para adicionar o filme
 	}
 
 	public void apagarFilme(int numero) {
-		System.out.println("Método por desenvolver");
+		filmes.remove(numero - 1);
 		// Completa o método para apagar o filme
 		
 	}
 	
-	public void editarFilme(int numero, String novoTitulo) {
+	public void editarFilme(int numero, String novoTitulo, int novoAno) {
 		// Falta acrescentar o atributo ano
-		filmes.get(numero - 1).setFilme(novoTitulo);
+		filmes.get(numero - 1).setFilme(novoTitulo, novoAno);
 		
 		
 	}
